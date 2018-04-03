420d5ae8e7c146f6bd30ec68dad2381873bf4392
codeekage <agiriabrahamjunior@gmail.com>
2018-04-03 15:37:28 +0100

    modified attend.handlebars

--- routes/attend.js
+++ routes/attend.js
@@ -1,12 +1,38 @@
 const express = require("express"),
-    routes = express.Router();
+    routes = express.Router(),
+    EventModel = require("../models/events.model")
 
 
 routes.get("/attend/:link", (req, res, next) => {
-    res.render("attend", {
-        layout : false
-    })
+    EventModel.findOne({evt_link : req.params.link}).then((event) => {
+        if(!event){
+            res.status(404).render("404",{
+                layout : false
+            });
+        }else{
+            res.render("attend", {
+                layout: false,
+                evt_link : event.evt_link
+            });
+        }
+    }).catch(next);
+ 
 });
+
+routes.post("/attend/:link", (req, res, next) => {
+    EventModel.findOne({evt_passkey : req.body.passkey}).then((event) => {
+        if(!event){
+            res.send({
+                success : false
+            })
+        }else{
+            res.send({
+                success : true,
+                event : event
+            })
+        }
+    })
+})
 
 
 
